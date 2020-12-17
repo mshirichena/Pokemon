d8f801f6b9d15dfa0adcb119e34f8bcd6c334fdc
Christian Shirichena <christianshirichena@Christians-MacBook-Pro.local>
2020-12-16 19:31:43 -0500

    Update

---
+++ Pokemon/PaginatedPokemon.swift
@@ -0,0 +1,36 @@
+//
+//  PaginatedPokemon.swift
+//  Pokemon
+//
+//  Created by Christian Shirichena on 12/16/20.
+//
+
+import Foundation
+
+struct PaginatedPokemon: Decodable {
+    let next: String
+    let results: [Result]
+
+    enum CodingKeys: String, CodingKey {
+        case next
+        case results
+    }
+}
+
+struct Result: Decodable {
+    let name: String
+    let url: String
+    
+   
+    enum ResultCodingKeys: String, CodingKey {
+        case name
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ResultCodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.url = try container.decode(String.self, forKey: .url)
+        
+    }
+}
--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     var pokemonArray: [Pokemon] = []  // declaring an array for Pokemon on tableview
     var limit = 30 // limit of sprites per page
     let totalEntries = 150 // upper limit of pokemon sprites
-    
+    var pokemonBatch: [Result] = []
+    var nextURL: String = ""
+    //        var offset = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +28,46 @@ class ViewController: UIViewController {
         self.PokemonTableView.dataSource = self
         self.PokemonTableView.delegate = self
         self.getThirtyPokemon() // Loadview, what displays on view controller
+        //                self.getAllPokemon()
+        self.getInitialData()
     }
     
+    func getInitialData() {
+        pokemonBatch = []
+        nextURL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=30"
+        getDataFromPokemon()
+    }
+    func getDataFromPokemon() {
+        if isArrayFull() == false {
+            guard let urlObj = URL(string: nextURL) else {return}
+            URLSession.shared.dataTask(with: urlObj) {[weak self](data, response, error) in
+                guard let data = data else {return}
+                do {
+                    let downloadedPokemon = try JSONDecoder().decode(PaginatedPokemon.self, from: data)
+                    self?.pokemonBatch.append(contentsOf: downloadedPokemon.results)
+                    self?.nextURL = downloadedPokemon.next
+                    
+                    DispatchQueue.main.async {
+                        self?.PokemonTableView.reloadData()
+                    }
+                }
+                catch {
+                    print(error)
+                }
+            }
+            .resume()
+        }
+    }
+    
+//        func createPokemonURL() -> String {
+//            let apiLink = "https://pokeapi.co/api/v2/pokemon/"
+//            return apiLink
+//        }
     private func getThirtyPokemon(){
         self.PokemonTableView.register(UINib(nibName: "PokemonTableViewCell", bundle: nil), forCellReuseIdentifier: "PokemonTableViewCell")
         self.PokemonTableView.dataSource = self
         let group = DispatchGroup() // Dispatch Group  - displays 30 Pokemon at a time
-        for _ in 1...30 {
+        for _ in 1...151 {
             group.enter()
             NetworkingManager.shared.getDecodableObject(from: self.createPokemonURL()) {  (pokemon: Pokemon?, error) in
                 guard let pokemon = pokemon else {return}
@@ -45,7 +80,12 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    func isArrayFull() -> Bool {
+        if pokemonBatch.count < totalEntries { return false}
+        else {
+            return true
+        }
+    }
     
     private func createPokemonURL () -> String {
         let randomNumber = Int.random(in: 1...151)
@@ -68,15 +108,26 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.pokemonArray.count
+        return self.pokemonBatch.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let count = pokemonBatch.count
+        let lastElement = count - 1
+        if indexPath.row == lastElement {
+            getDataFromPokemon()
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonTableViewCell", for: indexPath) as!
             PokemonTableViewCell
         cell.configure(with: self.pokemonArray[indexPath.row])
         return  cell
     }
+    
+    
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 44.0
+        }
 }
 extension ViewController: UITableViewDataSourcePrefetching {
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
@@ -88,7 +139,14 @@ extension ViewController: UITableViewDataSourcePrefetching {
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         performSegue(withIdentifier: "DetailsScreen", sender: self) // connects pokemon details viewcontroller with main view controller.
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        //let lastIndexPath = indexPath.row
+        if indexPath.row == pokemonArray.count - 1 {
+            if pokemonArray.count < totalEntries {
+                //self.getAllPokemon(pageNumber)
+            }
+        }
     }
 }
